5ef77e1ed22aebf34bac0b44abd424563c02acdb
alexbass3042 <83875018+alexbass3042@users.noreply.github.com>
2023-03-22 15:22:48 -0500

    22032023

--- Persona.java
+++ Persona.java
@@ -1,104 +1,65 @@
 package com.example.project2;
-
-    /*Crear clase Persona.
-    Crear variables las privadas edad, nombre y telefono de la clase Persona.
-    Crear gets y sets de cada propiedad.
-    Crear un objeto persona en el main.
-    Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono,
-     por último muéstralas por consola.   */
-
+/*
+Crea una clase Persona con las siguientes variables: La edad El nombre El teléfono
+Una vez creada la clase, crea una nueva clase Cliente que herede de Persona,
+esta nueva clase tendrá la variable credito solo para esa clase.
+Crea ahora un objeto de la clase Cliente que debe tener como propiedades la edad,
+el telefono, el nombre y el credito, tienes que darles valor y mostrarlas por pantalla.
+Una vez hecho esto, haz lo mismo con la clase Trabajador que herede de Persona, y con
+una variable salario que solo tenga la clase Trabajador.
+*/
 public class Persona {
     private int edad;
     private String nombre;
     private int tlf;
 
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public void setTlf(int tlf) {
-        this.tlf = tlf;
-    }
+    public void setEdad(int edad) {this.edad = edad;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setTlf(int tlf) {this.tlf = tlf;}
 
-    public int getEdad() {
-        return this.edad;
-    }
-    public String getNombre() {
-        return this.nombre;
-    }
-    public int getTlf() {
-        return this.tlf;
-    }
+    public int getEdad() {return this.edad;}
+    public String getNombre() {return this.nombre;}
+    public int getTlf() {return this.tlf;}
 
     public static void main(String[] args) {
-    Persona persona = new Persona();
-    persona.setEdad(20);
-    persona.setNombre("alex");
-    persona.setTlf(98765432);
-    System.out.println();
-    System.out.println("Nombre es: " + persona.getNombre() + ", Edad: " + persona.getEdad()+ ", Tlf: " + persona.getTlf());
-    System.out.println();
-    System.out.println("Nombre: " + persona.getNombre());
-    System.out.println("Edad: " + persona.getEdad());
-    System.out.println("Tlf: " + persona.getTlf());
+        Cliente cliente = new Cliente();
+        cliente.setEdad(20);
+        cliente.setNombre("Alex");
+        cliente.setTlf(98765432);
+        cliente.setCredito("SI");
+
+        System.out.println("Nombre es: " + cliente.getNombre() + ", Edad: " + cliente.getEdad()+ ", Tlf: " + cliente.getTlf()+ ", Credito:" + cliente.getCredito());
+        System.out.println();
+        System.out.println("Nombre: " + cliente.getNombre());
+        System.out.println("Edad: " + cliente.getEdad());
+        System.out.println("Tlf: " + cliente.getTlf());
+        System.out.println("Credito:" + cliente.getCredito());
+        System.out.println();
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.setNombre("Alex");
+        trabajador.setEdad(Integer.parseInt("40"));
+        trabajador.setTlf(998899);
+        trabajador.setSalario(Integer.parseInt("1200"));
+
+        System.out.println("Nombre es: " + trabajador.getNombre() + ", Edad: " + trabajador.getEdad()+ ", Tlf: " + trabajador.getTlf()+ ", Salario:" + trabajador.getSalario());
+        System.out.println("Nombre: " + trabajador.getNombre());
+        System.out.println("Edad: " + trabajador.getEdad());
+        System.out.println("Tlf: " + trabajador.getTlf());
+        System.out.println("Salario: $" + trabajador.getSalario());
+    }
+static class Cliente extends Persona{
+    private String credito;
+    public void setCredito(String credito) {
+        this.credito = credito;
+    }
+    public String getCredito(){
+        return this.credito;
     }
 }
-
-
-
-
-
-
-
-/*
-            int numeroif = 0;
-        if (numeroif < 0) {
-            System.out.println("El numero es negativo");
-        } else if (numeroif > 0) {
-            System.out.println("El numero es positivo");
-        } else {
-            System.out.println("El numero es cero");
-        }
-
-        // Paso 2 incrementa numero
-        int NumeroW = 2;
-        while (NumeroW < 3) {
-            System.out.println("El numero es: " + NumeroW);
-            NumeroW++;
-        }
-        System.out.println("El numero es mayor");
-
-        // Paso 3
-        int Numerodw = 3;
-        do {
-            System.out.println("El numero es: " + Numerodw);
-            Numerodw++;
-        } while (Numerodw < 3);
-        System.out.println("El numero es mayor");
-
-        // Paso 4
-        for (int numeroFor = 3; numeroFor <= 3; numeroFor++) {
-            System.out.println("Respuesta For: " + numeroFor);
-        }
-        System.out.println("El numero es mayor");
-
-        // Paso 5
-        String estacion = "otoño";
-        switch (estacion) {
-            case "verano":
-                System.out.println("Es: verano");
-                break;
-            case "invierno":
-                System.out.println("Es: invierno");
-                break;
-            case "otoño":
-                System.out.println("Es: otoño");
-                break;
-            case "primavera":
-                System.out.println("Es: primavera");
-                break;
-            default:
-                System.out.println("No es una estación");
-  */
+static class Trabajador extends Persona{
+    private int salario;
+    public void setSalario(int salario) {this.salario = salario;}
+    public int getSalario(){return this.salario;}
+}
+}
