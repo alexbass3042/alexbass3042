f17b7e99f44737fc24669eccce2d6ec7cc06e1aa
alexbass3042 <83875018+alexbass3042@users.noreply.github.com>
2023-05-02 09:10:57 -0500

    Tipos de datos en JAVA
    declarar variables e imprimir por pantalla.

---
+++ Java0Basic/src/JavaCero/Java01.java
@@ -0,0 +1,23 @@
+package JavaCero;
+
+public class Java01 {
+
+    public static void main(String[] args) {
+        //tipos de datos más comunes son int, long, double, boolean, String
+        //Declarando variables
+        int entero = 10;
+        long largo = 100000L;
+        double doble = 3.1416;
+        boolean booleano = true;
+        String cadena = "Hola mundo";
+
+        //imprimimos los tipos de datos por pantalla
+        System.out.println("Estos son los tipos de mas comunes en JAVA..");
+        System.out.println("Tipo de dato Entero: " + entero);
+        System.out.println("Tipo de dato Largo: " + largo);
+        System.out.println("Tipo de dato Doble: " + doble);
+        System.out.println("Tipo de dato Booleano: " + booleano);
+        System.out.println("Tipo de dato String: " + cadena);
+
+    }
+}
